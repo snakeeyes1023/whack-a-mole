fdb3ccc8b31d41c915b94386416093aa4e1fc693
Jonathan Côté <55141844+snakeeyes1023@users.noreply.github.com>
2022-10-06 10:57:57 -0400

    Redirection game over

--- lib/src/views/gameoverView.dart
+++ lib/src/views/gameoverView.dart
@@ -10,10 +10,12 @@ import '../helper/colorHelper.dart';
 import 'homeView.dart';
 
 class GameOverView extends StatefulWidget {
-  GameOverView({super.key});
+  GameOverView(int score, {key}) : super(key: key) {
+    this.score = score;
+  }
 
   Random randomInstance = Random();
-  String scoreExists = '';
+  late int score = 0;
 
   TextEditingController nameController = TextEditingController();
 
@@ -22,111 +24,112 @@ class GameOverView extends StatefulWidget {
 }
 
 class _GameoverViewState extends State<GameOverView> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: const Color.fromARGB(255, 16, 28, 79),
         body: Padding(
-      padding: const EdgeInsets.all(5),
-      child: Container(
-          width: double.infinity,
-          height: double.infinity,
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-          repeat: ImageRepeat.repeat,
-        image: Svg('assets/images/backgroundPattern.svg', size: Size.square(35)),
-        colorFilter: ColorFilter.mode(Color.fromARGB(255, 33, 82, 150), BlendMode.modulate),
-        )),
-        child: Column(children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 60, left: 400, right: 400),
-            //master text and some text
-            child: Center(
-                child: SingleChildScrollView(
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    const Center(
-                        child: Image(image: Svg('images/bugIcon.svg', size: Size(50, 50)) )),
-                    const Text(
-                      'GAME OVER',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    const Text(
-                      'YOU LOST',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    const Padding(
-                        padding: EdgeInsets.symmetric(vertical: 20),
-                        child: Divider(
-                          color: Colors.white,
-                          thickness: 1,
-                        )),
-                    const Text(
-                      'SCORE',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    /* 
-                    *
-                    *
-                    state du score 
-                    *
-                    *
-                    */
-                    const Padding(
-                        padding: EdgeInsets.symmetric(vertical: 20),
-                        child: Divider(
-                          color: Colors.white,
-                          thickness: 1,
-                        )),
-                    const Text(
-                      'TRY AGAIN?',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Center(
-                  child: RipplesAnimation(
-                    color: Colors.white,
-                    onPressed: () => Navigator.pop(context),
-                    key: widget.key,
-              )),
-                    CustomInputField(
-                      fieldController: widget.nameController,
-                      text: 'NAME:',
-                    ),
-                    Padding(
-                  padding: const EdgeInsets.only(top: 10),
-                  child: Center(
-                      child: LinkButton(
-                    text: 'SAVE SCORE',
-                    onClick: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const HomeView()),
-                      );
-                    },
-                  ))),
-                  ]),
-            )),
-          )
-        ]))));
+            padding: const EdgeInsets.all(5),
+            child: Container(
+                width: double.infinity,
+                height: double.infinity,
+                decoration: const BoxDecoration(
+                    image: DecorationImage(
+                  repeat: ImageRepeat.repeat,
+                  image: Svg('assets/images/backgroundPattern.svg',
+                      size: Size.square(35)),
+                  colorFilter: ColorFilter.mode(
+                      Color.fromARGB(255, 33, 82, 150), BlendMode.modulate),
+                )),
+                child: Column(children: [
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(top: 60, left: 400, right: 400),
+                    //master text and some text
+                    child: Center(
+                        child: SingleChildScrollView(
+                      child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            const Center(
+                                child: Image(
+                                    image: Svg('images/bugIcon.svg',
+                                        size: Size(50, 50)))),
+                            const Text(
+                              'GAME OVER',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 30,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            const Text(
+                              'YOU LOST',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            const Padding(
+                                padding: EdgeInsets.symmetric(vertical: 20),
+                                child: Divider(
+                                  color: Colors.white,
+                                  thickness: 1,
+                                )),
+                            const Text(
+                              'SCORE',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              widget.score.toString(),
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 30,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            const Padding(
+                                padding: EdgeInsets.symmetric(vertical: 20),
+                                child: Divider(
+                                  color: Colors.white,
+                                  thickness: 1,
+                                )),
+                            const Text(
+                              'TRY AGAIN?',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Center(
+                                child: RipplesAnimation(
+                              color: Colors.white,
+                              onPressed: () => Navigator.pop(context),
+                              key: widget.key,
+                            )),
+                            CustomInputField(
+                              fieldController: widget.nameController,
+                              text: 'NAME:',
+                            ),
+                            Padding(
+                                padding: const EdgeInsets.only(top: 10),
+                                child: Center(
+                                    child: LinkButton(
+                                  text: 'SAVE SCORE',
+                                  onClick: () {
+                                    Navigator.pop(context);
+                                  },
+                                ))),
+                          ]),
+                    )),
+                  )
+                ]))));
   }
 }
